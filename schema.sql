92a2cb2040faed92d1b621e7c305f2b4ea0755a3
leomosley <leo.mosley2@gmail.com>
2024-10-03 10:40:55 +0100

    init commit

---
+++ schema.sql
@@ -0,0 +1,5 @@
+/*
+  Group 26 DBPRIN Coursework Database Schema
+  ERD PDF: https://drive.google.com/file/d/1nB0oTYydLEmE2_XtO4u9A2eawc3cBWs-/view?usp=drive_link
+  Github Repo: https://github.com/leomosley/dbprin-cw
+*/
