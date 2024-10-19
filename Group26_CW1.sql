fc429c96d1a0bd5a0b6a8037582bdbd1e864436b
Tomas Vincent <91481444+UP933272@users.noreply.github.com>
2024-10-19 20:34:09 +0100

    all tables have been created feel free to check or edit for mistakes

--- Group26_CW1.sql
+++ Group26_CW1.sql
@@ -11,7 +11,8 @@ CREATE TABLE
     branch_addr1 VARCHAR(150),
     branch_addr2 VARCHAR(150),
     branch_postcode VARCHAR(10),
-    branch_contact_number VARCHAR(15) branch_email VARCHAR(150) NOT NULL
+    branch_contact_number VARCHAR(15),
+    branch_email VARCHAR(150) NOT NULL,
   );
 
 CREATE TABLE
@@ -135,7 +136,7 @@ CREATE TABLE
     dep_id INT NOT NULL,
     course_id INT NOT NULL,
     PRIMARY KEY (dep_id, course_id),
-    CONSTRAINT fk_department FOREIGN KEY (dep_id) REFERENCES staff (staff_id),
+    CONSTRAINT fk_department FOREIGN KEY (dep_id) REFERENCES department (dep_id),
     CONSTRAINT fk_course FOREIGN KEY (course_id) REFERENCES course (course_id)
   );
 
@@ -154,7 +155,9 @@ CREATE TABLE
   room (
     room_id serial PRIMARY KEY NOT NULL,
     building_id INT,
-    CONSTRAINT fk_building_id FOREIGN KEY (building_id) REFERENCES building (building_id) room_name VARCHAR(100) NOT NULL room_alt_name VARCHAR(100) NOT NULL,
+    CONSTRAINT fk_building_id FOREIGN KEY (building_id) REFERENCES building (building_id),
+    room_name VARCHAR(100) NOT NULL,
+    room_alt_name VARCHAR(100) NOT NULL,
     room_type VARCHAR(100) NOT NULL,
     room_capacity INT NOT NULL
   );
@@ -196,7 +199,7 @@ CREATE TABLE
     facility_total_quantity INT,
     facility_name VARCHAR(100) NOT NULL,
     facility_description TEXT,
-    facility_notes TEXT NOT NULL,
+    facility_notes TEXT NOT NULL
   );
 
 CREATE TABLE
@@ -204,7 +207,7 @@ CREATE TABLE
     hour_id serial PRIMARY KEY NOT NULL,
     start_time TIME NOT NULL,
     end_time TIME NOT NULL,
-    date DATE NOT NULL,
+    date DATE NOT NULL
   );
 
 CREATE TABLE
@@ -213,8 +216,7 @@ CREATE TABLE
     session_id INT NOT NULL,
     PRIMARY KEY (teacher_id, session_id),
     CONSTRAINT fk_teacher FOREIGN KEY (teacher_id) REFERENCES teacher (teacher_id),
-    CONSTRAINT fk_session FOREIGN KEY (session_id) REFERENCES session (session_id),
-    quantity INT NOT NULL
+    CONSTRAINT fk_session FOREIGN KEY (session_id) REFERENCES session (session_id)
   );
 
 CREATE TABLE
@@ -243,7 +245,7 @@ CREATE TABLE
     attendance_record BOOLEAN NOT NULL
   );
 
-  CREATE TABLE
+CREATE TABLE
   student_contact (
     contact_id INT NOT NULL,
     student_id INT NOT NULL,
@@ -252,7 +254,7 @@ CREATE TABLE
     CONSTRAINT fk_student FOREIGN KEY (student_id) REFERENCES student (student_id)
   );
 
-  CREATE TABLE
+CREATE TABLE
   student (
     student_id SERIAL PRIMARY KEY,
     staff_edu_email CHAR(22),
@@ -269,11 +271,11 @@ CREATE TABLE
     student_landline VARCHAR(30),
     student_mobile VARCHAR(15) NOT NULL,
     student_dob DATE NOT NULL,
-    student_attendance DECIMAL(5,2) NOT NULL,
+    student_attendance DECIMAL(5, 2) NOT NULL,
     UNIQUE (student_edu_email, student_number)
   );
 
-  CREATE TABLE
+CREATE TABLE
   student_tuition (
     student_id INT NOT NULL,
     tuition_id INT NOT NULL,
@@ -285,28 +287,28 @@ CREATE TABLE
 CREATE TABLE
   tuition (
     tuition_id SERIAL PRIMARY KEY NOT NULL,
-    tuition_amount DECIMAL(7,2) NOT NULL,
-    tuition_paid DECIMAL(7,2) NOT NULL,
-    tuition_remaining DECIMAL(7,2) NOT NULL,
-    tuition_remaining_perc DECIMAL(5,2),
+    tuition_amount DECIMAL(7, 2) NOT NULL,
+    tuition_paid DECIMAL(7, 2) NOT NULL,
+    tuition_remaining DECIMAL(7, 2) NOT NULL,
+    tuition_remaining_perc DECIMAL(5, 2),
     tuition_deadline DATE NOT NULL
   );
 
-  CREATE TABLE
+CREATE TABLE
   tuition_payment (
     tuition_payment_id SERIAL PRIMARY KEY NOT NULL,
     tuition_payment_reference CHAR(12) NOT NULL,
-    tuition_payment_amount DECIMAL(7,2) NOT NULL,
+    tuition_payment_amount DECIMAL(7, 2) NOT NULL,
     tuition_payment_date DATE NOT NULL,
     tuition_payment_method VARCHAR(50) NOT NULL,
     UNIQUE (tuition_payment_reference)
   );
 
-  CREATE TABLE
+CREATE TABLE
   student_course (
     student_id INT NOT NULL,
     course_id INT NOT NULL,
-    PRIMARY KEY (student_id, tuition_id),
+    PRIMARY KEY (student_id, course_id),
     CONSTRAINT fk_student FOREIGN KEY (student_id) REFERENCES student (student_id),
     CONSTRAINT fk_course FOREIGN KEY (course_id) REFERENCES course (course_id)
   );
@@ -324,18 +326,107 @@ CREATE TABLE
   student_course_progression (
     student_id INT NOT NULL,
     course_id INT NOT NULL,
-    PRIMARY KEY (student_id, tuition_id),
+    PRIMARY KEY (student_id, course_id),
     CONSTRAINT fk_student FOREIGN KEY (student_id) REFERENCES student (student_id),
     CONSTRAINT fk_course FOREIGN KEY (course_id) REFERENCES course (course_id),
-    progress_perc DECIMAL(5,2) NOT NULL,
+    progress_perc DECIMAL(5, 2) NOT NULL,
     feedback TEXT,
-    culmative_average DECIMAL(5,2) NOT NULL
+    culmative_average DECIMAL(5, 2) NOT NULL
   );
-  CREATE TABLE
+
+CREATE TABLE
   course_module (
     course_id INT NOT NULL,
     module_id INT NOT NULL,
     PRIMARY KEY (course_id, module_id),
-    CONSTRAINT fk_student FOREIGN KEY (student_id) REFERENCES student (student_id),
+    CONSTRAINT fk_course FOREIGN KEY (course_id) REFERENCES course (course_id),
+    CONSTRAINT fk_module FOREIGN KEY (module_id) REFERENCES module (module_id)
+  );
+
+CREATE TABLE
+  course_coordinator (
+    teacher_id INT NOT NULL,
+    course_id INT NOT NULL,
+    PRIMARY KEY (teacher_id, course_id),
+    CONSTRAINT fk_teacher FOREIGN KEY (teacher_id) REFERENCES teacher (teacher_id),
     CONSTRAINT fk_course FOREIGN KEY (course_id) REFERENCES course (course_id)
+  );
+
+CREATE TABLE
+  module_coordinatior (
+    teacher_id INT NOT NULL,
+    module_id INT NOT NULL,
+    PRIMARY KEY (teacher_id, module_id),
+    CONSTRAINT fk_teacher FOREIGN KEY (teacher_id) REFERENCES teacher (teacher_id),
+    CONSTRAINT fk_module FOREIGN KEY (module_id) REFERENCES module (module_id)
+  );
+
+CREATE TABLE
+  student_module_progression (
+    student_id INT NOT NULL,
+    module_id INT NOT NULL,
+    PRIMARY KEY (student_id, module_id),
+    CONSTRAINT fk_student FOREIGN KEY (student_id) REFERENCES student (student_id),
+    CONSTRAINT fk_module FOREIGN KEY (module_id) REFERENCES module (module_id),
+    module_grade VARCHAR(50) NOT NULL,
+    feedback TEXT,
+    passed BOOLEAN NOT NULL
+  );
+
+CREATE TABLE
+  module (
+    module_id SERIAL PRIMARY KEY NOT NULL,
+    module_code INT NOT NULL,
+    module_name VARCHAR(50) NOT NULL,
+    module_description TEXT,
+    academ_lvl CHAR(2) NOT NULL,
+    module_credits INT NOT NULL,
+    module_status VARCHAR(20) NOT NULL,
+    last_reviewed DATE NOT NULL,
+    notional_hours DECIMAL(5, 2) NOT NULL,
+    method_of_delivery ENUM NOT NULL,
+    module_duration INT NOT NULL,
+    UNIQUE (module_code)
+  );
+
+CREATE TABLE
+  student_assessment_attempt (
+    student_id INT NOT NULL,
+    attempt_id INT NOT NULL,
+    PRIMARY KEY (student_id, attempt_id),
+    CONSTRAINT fk_student FOREIGN KEY (student_id) REFERENCES student (student_id),
+    CONSTRAINT fk_assessment_attempt FOREIGN KEY (attempt_id) REFERENCES assessment_attempt (attempt_id),
+    highest_grade DECIMAL(5, 2) NOT NULL,
+    feedback TEXT,
+    attempts INT
+  );
+
+CREATE TABLE
+  assessment_attempt (
+    attempt_id serial PRIMARY KEY NOT NULL,
+    assessment_id INT,
+    CONSTRAINT fk_assessment_id FOREIGN KEY (assessment_id) REFERENCES assessment (assessment_id),
+    attempt_grade DECIMAL(5, 2) NOT NULL,
+    attempt_feedback TEXT,
+    attempt_no INT NOT NULL,
+    attempt_date DATE NOT NULL,
+    attempt_time TIME NOT NULL
+  );
+
+CREATE TABLE
+  assessment (
+    assessment_id serial PRIMARY KEY NOT NULL,
+    module_id INT,
+    CONSTRAINT fk_module_id FOREIGN KEY (module_id) REFERENCES module (module_id),
+    assessment_title VARCHAR(50) NOT NULL,
+    assessment_set_date DATE NOT NULL,
+    assessment_set_time TIME attempt_no INT NOT NULL,
+    assessment_due_date DATE NOT NULL,
+    assessment_due_time TIME NOT NULL,
+    assessment_description TEXT,
+    assessment_type VARCHAR(100) NOT NULL,
+    assessment_weighting DECIMAL(5, 2) NOT NULL,
+    assessment_attachment TEXT,
+    assessment_max_attempts INT NOT NULL,
+    assessment_visble BOOLEAN NOT NULL
   );
